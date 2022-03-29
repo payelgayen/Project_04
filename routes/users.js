43cc645b47f52c96191bc175b4bfafa433d8c7e8
Daniela <dani.calderon1093>
2022-03-29 16:23:09 +1100

    step 3

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,22 @@ const express = require("express");
 const router = express.Router();
 
 // GET all users
-router.get("/", (req, res) => {});
+app.get('/users', (req,res) => {
+  db.any('SELECT * FROM users')
+  .then((users) => {
+      // if success;
+      console.log(users)
+
+      res.render('pages/users',
+      {users,
+      title: 'ALL Users'})
+  })
+  .catch((error) => {
+      // error;
+      console.log(error)
+      res.redirect("/error?message=" + error.message)
+  });
+});
 
 // GET specific users
 router.get("/:user_id", (req, res) => {
@@ -12,6 +27,27 @@ router.get("/:user_id", (req, res) => {
   db.oneOrNone("SELECT * FROM users WHERE id = $1", [index]);
   // const user = users[index];
   //then;
+
+
+
+db.any('SELECT * FROM users')
+.then((users) => {
+  // if success;
+const index = req.params.user_id;
+const user = users[index];
+
+// validation to confirm number has been entered
+if (index >= users.length) {
+  res.status(400).send(`msg: User ${index} is not found`);
+}
+res.render('pages/user', {user, title: 'User' })
+
+})
+.catch((error) => {
+// error;
+console.log(error)
+res.redirect("/error?message=" + error.message)
+});
 });
 
 module.exports = router;
